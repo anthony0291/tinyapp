71ce8bec78a52d9d70dd2d4888a9db3fc98c2bca
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-06-12 20:13:16 +0000

    added urls_new

--- express_server.js
+++ express_server.js
@@ -3,12 +3,14 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
+function generateRandomString() {};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,8 +33,18 @@ app.get("/urls", (request, response) => {
   response.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (request, response) => {
   const templateVars = {id: request.params.id, longURL: "http://www.lighthouselabs".ca};
   response.render("urls_show", templateVars);
 });
 
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
