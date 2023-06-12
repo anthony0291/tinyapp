4bf1059b652fe987f72e7f00278c7df14654c0ec
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-06-12 17:54:33 +0000

    added hello request

--- express_server.js
+++ express_server.js
@@ -17,8 +17,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
