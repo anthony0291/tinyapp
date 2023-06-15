a7fcc491893b2c15524867eeecc2d08266561af6
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-06-15 18:17:28 +0000

    fixed Tiny url tagline

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // Lesson: Template Engine-EJS
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
@@ -50,14 +50,20 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//2
+//(173-hello_world.ejs)
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  //    ^^^^^^^^ localhost:8080/hello
+  const templateVars = { greeting: "Hello Bob" };
+  //        ^^^ set templateVars to obj with greeting key(go to ejs1)
+  res.render("hello_world", templateVars);
+  //        (hello_world.ejs, passing in templateVars);
 });
 
-//INDEX.ejs
+
+//3 INDEX.ejs (173)
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
+  console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
@@ -66,9 +72,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-//SHOW.ejs
+
+
+//SHOW.ejs (Lesson/173)
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {id: req.params.id, longURL: "http://www.lighthouselabs".ca};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
@@ -83,12 +91,12 @@ app.get("/urls/:id", (req, res) => {
 // });
 
 //Redirects from /u/:id to actual site.
-app.get("/u/:id", (req, res) => {
-  // console.log(urlDatabase[req.params.id]);
-  const longURL = urlDatabase[req.params.id];
-  console.log(longURL);
-  res.redirect(`${longURL}`);
-});
+// app.get("/u/:id", (req, res) => {
+//   // console.log(urlDatabase[req.params.id]);
+//   const longURL = urlDatabase[req.params.id];
+//   console.log(longURL);
+//   res.redirect(`${longURL}`);
+// });
 
 // ************  POST  ******************* */
 
